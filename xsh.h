8c60e04f103a4f7c2b3367cec7dd52519caf9cc2
yujinsong <88403596@qq.com>
2017-10-31 17:00:30 +0800

    add ls

---
+++ xsh.h
@@ -0,0 +1,17 @@
+#ifndef XSH_H
+#define XSH_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+// buffer size
+#define BUFFERSIZE 4096
+// file perm
+#define MODE 0644
+
+#endif
+
